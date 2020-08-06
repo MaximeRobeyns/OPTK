7fc38071ff0448f991f3e7725de16dba57dc850c
Maxime Robeyns <maximerobeyns@gmail.com>
2020-08-06 21:54:47 +0100

    further progress on types and tests

--- src/types.cpp
+++ src/types.cpp
@@ -22,6 +22,9 @@
 #include <optk/types.hpp>
 #include <random>
 
+/// TODO get rid of this
+#include <iostream>
+
 // choice ----------------------------------------------------------------------
 
 optk::choice::choice (std::string n, std::vector<optk::param_t *> *options)
@@ -57,9 +60,9 @@ optk::randint::randint (std::string n, int l, int u)
 {
     m_name = n;
     m_type = param::randint;
-    // initialise mersenne twister prng using the random device.
-    std::mt19937 generator(rd());
-    std::uniform_int_distribution<int> dist (l, u);
+    // initialise Mersenne twister prng using the random device.
+    generator = std::mt19937 (rd());
+    dist = std::uniform_int_distribution<int> (l, u);
 }
 
 int
@@ -73,15 +76,132 @@ optk::randint::sample()
 optk::uniform::uniform (std::string n, double l, double u)
 {
     m_name = n;
+    m_lower = l;
+    m_upper = u;
     m_type = param::uniform;
-    std::mt19937 generator(rd());
-    std::uniform_real_distribution<double> dist (l, u);
+    generator = std::mt19937 (rd());
+    dist = std::uniform_real_distribution<double> (l, u);
 }
 
 double
 optk::uniform::sample()
 {
     return dist (generator);
+}
+
+// quniform --------------------------------------------------------------------
+
+optk::quniform::quniform (std::string n, double l, double u, double q):
+    uniform (n, l, u)
+{
+    m_q = q;
+    m_type = param::quniform;
+}
+
+double
+optk::quniform::sample ()
+{
+    double value = round(dist (generator) / m_q) * m_q;
+    if (value < m_lower) {
+        value = m_lower;
+    } else if (value > m_upper) {
+        value = m_upper;
+    }
+
+    return value;
+}
+
+// loguniform ------------------------------------------------------------------
+
+optk::loguniform::loguniform (std::string n, double l, double u):
+    uniform (n, l, u)
+{
+    m_type = param::loguniform;
+
+   if (l <= 0 || u <= 0) {
+        throw std::invalid_argument ("bounds cannot be negative or zero");
+    }
+
+    dist = std::uniform_real_distribution<double> (log (l), log (u));
+}
+
+double
+optk::loguniform::sample ()
+{
+    return exp (dist (generator));
+}
+
+// qloguniform ----------------------------------------------------------------
+
+optk::qloguniform::qloguniform (
+    std::string n,
+    double lower,
+    double upper,
+    double q
+) :
+    loguniform (n, lower, upper)
+{
+    m_type = param::qloguniform;
+    m_q = q;
+}
+
+double
+optk::qloguniform::sample ()
+{
+    double value = round(optk::loguniform::sample()/m_q) * m_q;
+
+    if (value < m_lower) {
+        value = m_lower;
+    } else if (value > m_upper) {
+        value = m_upper;
+    }
+
+    return value;
+}
+
+// normal ----------------------------------------------------------------------
+
+optk::normal::normal (std::string n, double mu, double sigma)
+{
+    m_type = param::normal;
+    m_name = n;
+    generator = std::mt19937 (rd());
+    dist = std::normal_distribution<double> (mu, sigma);
+}
+
+double
+optk::normal::sample ()
+{
+    return dist (generator);
+}
+
+// qnormal ---------------------------------------------------------------------
+
+optk::qnormal::qnormal (std::string n, double mu, double sigma, double q) :
+    normal (n, mu, sigma)
+{
+    m_type = param::qnormal;
+    m_q = q;
+}
+
+double
+optk::qnormal::sample ()
+{
+    return round(normal::sample () / m_q) * m_q;
+}
+
+// lognormal -------------------------------------------------------------------
+
+optk::lognormal::lognormal (std::string n, double mu, double sigma) :
+    normal (n, mu, sigma)
+{
+    m_type = param::lognormal;
+}
+
+double
+optk::lognormal::sample ()
+{
+    return exp(normal::sample());
 }
 
 
--- test/types_test.cpp
+++ test/types_test.cpp
@@ -22,6 +22,29 @@
 #include <tests/types_test.hpp>
 #include <optk/types.hpp>
 
+static void
+test_categorical()
+{
+    std::vector<int> myopts;
+    for (int i = 0; i < 5; i++) {
+        myopts.push_back (i);
+    }
+
+    optk::categorical<int> test ("int categorical", &myopts);
+
+    assert (test.get_name () == std::string ("int categorical"));
+    assert (test.get_type () == param::categorical);
+    assert (test.count () == myopts.size ());
+
+    std::vector<int> *ret = test.values ();
+    assert (ret->size () == myopts.size ());
+
+    for (int i = 0; i < 5; i++) {
+        assert (test.get (i) == i);
+    }
+}
+
+// TODO test the choice parameter type once others have been implemented
 /*
 static void test_choice_type() {
     optk::choice_opts options(5);
@@ -48,30 +71,143 @@ static void test_choice_type() {
 */
 
 static void
-test_categorical()
+test_randint ()
 {
-    std::vector<int> myopts;
-    for (int i = 0; i < 5; i++) {
-        myopts.push_back(i);
-    }
+    optk::randint testrandint("test", 0, 10);
 
-    optk::categorical<int> test("int categorical", &myopts);
 
-    assert (test.get_name() == std::string("int categorical"));
-    assert (test.get_type() == param::categorical);
-    assert (test.count() == myopts.size());
+    assert (testrandint.get_name () == std::string ("test"));
+    assert (testrandint.get_type () == param::randint);
 
-    std::vector<int> *ret = test.values();
-    assert(ret->size() == myopts.size());
-
-    for (int i = 0; i < 5; i++) {
-        assert (test.get(i) == i);
+    for (int i = 0; i < 100; i++) {
+        int tmp = testrandint.sample ();
+        assert (0 <= tmp && tmp <= 10);
     }
 }
 
-void run_type_tests() {
+static void
+test_uniform ()
+{
+    optk::uniform testuniform ("testing", 0, 10);
+
+    assert (testuniform.get_name () == std::string ("testing"));
+    assert (testuniform.get_type () == param::uniform);
+
+    for (int i = 0; i < 100; i++) {
+        double tmp = testuniform.sample ();
+        assert (0.0 <= tmp && tmp <= 10.0);
+    }
+}
+
+static void
+test_quniform ()
+{
+    optk::quniform testquniform ("testing", 0, 10, 2);
+
+    assert (testquniform.get_name () == std::string ("testing"));
+    assert (testquniform.get_type () == param::quniform);
+
+    for (int i = 0; i < 100; i++) {
+        double tmp = testquniform.sample ();
+        assert (0.0 <= tmp && tmp <= 10.0);
+        assert ((int) tmp % 2 == 0);
+    }
+}
+
+static void
+test_loguniform ()
+{
+    optk::loguniform tloguniform ("testing", 1, 10);
+
+    assert (tloguniform.get_name () == std::string ("testing"));
+    assert (tloguniform.get_type () == param::loguniform);
+
+    for (int i = 0; i < 100; i++) {
+        double tmp = tloguniform.sample ();
+        assert (1.0 <= tmp && tmp <= 10.0);
+    }
+}
+
+static void
+test_qloguniform ()
+{
+    optk::qloguniform testqlu ("test_qlu", 10, 20, 2);
+
+    assert (testqlu.get_name () == std::string ("test_qlu"));
+    assert (testqlu.get_type () == param::qloguniform);
+
+    for (int i = 0; i < 100; i++) {
+        double tmp = testqlu.sample ();
+        assert (10.0 <= tmp && tmp <= 20);
+        assert ((int) tmp % 2 == 0);
+    }
+}
+
+static void
+test_normal ()
+{
+    optk::normal testnorm ("testnormal", 5, 2.5);
+
+    assert (testnorm.get_name () == std::string ("testnormal"));
+    assert (testnorm.get_type () == param::normal);
+
+    int N = 500;
+
+    double total = 0;
+    double values[N];
+    for (int i = 0; i < N; i++) {
+        values[i] = testnorm.sample();
+        total += values[i];
+    }
+    double em_mean = total / N;
+    assert (4.5 < em_mean && em_mean < 5.5);
+    double tmp_em_var = 0;
+    for (int i = 0; i < N; i++) {
+        tmp_em_var += pow((values[i] - em_mean),2);
+    }
+    double em_var = sqrt(tmp_em_var / (N-1));
+    assert (2.2 < em_var && em_var < 2.8);
+}
+
+static void
+test_qnormal ()
+{
+    optk::qnormal testqnorm ("testqnorm", 10, 5, 2);
+
+    assert (testqnorm.get_name () == std::string ("testqnorm"));
+    assert (testqnorm.get_type () == param::qnormal);
+
+    for (int i = 0; i < 100; i++) {
+        assert ((int)testqnorm.sample() % 2 == 0);
+    }
+
+}
+
+static void
+test_lognormal ()
+{
+    optk::lognormal testlognorm ("testlognorm", 10, 5);
+
+    assert (testlognorm.get_name () == std::string ("testlognorm"));
+    assert (testlognorm.get_type () == param::lognormal);
+
+    // TODO possibly implement a normality test on the log of sampled values
+    // e.g. Kolmogorov–Smirnov test
+}
+
+void
+run_type_tests()
+{
     // test_choice_type();
-    test_categorical();
+    test_categorical ();
+    test_randint ();
+    test_uniform ();
+    test_quniform ();
+    test_loguniform ();
+    test_qloguniform ();
+    test_normal ();
+    test_qnormal ();
+    test_lognormal ();
     std::cout << "All type tests pass" << std::endl;
 }
 
